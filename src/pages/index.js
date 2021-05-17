d05cfc2f0e7e5be5fb3b38d9dc010de6ffc9ccc7
Taito <taito.tanaka1211@gmail.com>
2021-05-18 00:14:56 +0900

    modify card]

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,19 +13,28 @@ export default function Home() {
         <h1 className={styles.title}>
           What is Tabi?
         </h1>
-      </main>
 
-      <div class="min-h-screen min-w-screen bg-gray-100 flex items-center justify-center">
-  <div>
-    <div class="flex flex-col max-w-md bg-white px-8 py-6 rounded-xl space-y-5 items-center">
-      <h3 class="font-serif font-bold text-gray-900 text-xl">Fire Up Your Motivation</h3>
-      <img class="w-full rounded-md" src="https://coffeeordie.com/wp-content/uploads/2019/03/FraserCOVER2.jpg" alt="motivation" />
-      <p class="text-center leading-relaxed">In a thread on Reddit which has garnered nearly 10,000 replies, people who go to the gym every day (or practice some kind of other daily skill) have been sharing the things that help them stay motivated and proactive in keeping up with their good habits.</p>
-      <span class="text-center">By Matt Fraser</span>
-      <button class="px-24 py-4 bg-gray-900 rounded-md text-white text-sm focus:border-transparent">Read article</button>
-    </div>
-  </div>
-</div>
+        <div className="m-auto px-4 py-8 max-w-xl">
+          <div className="bg-white shadow-2xl" >
+              <div>
+                  <img src="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"></img>
+              </div>
+              <div className="px-4 py-2 mt-2 bg-white">
+                  <h2 className="font-bold text-2xl text-gray-800">Who uses Tailwind?</h2>
+                      <p className="sm:text-sm text-xs text-gray-700 px-2 mr-1 my-3">
+                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora reiciendis ad architecto at aut placeat quia, minus dolor praesentium officia maxime deserunt porro amet ab debitis deleniti modi soluta similique...
+                      </p>
+                  <div className="user flex items-center -ml-3 mt-8 mb-4">
+                      <div className="user-logo">
+                          <img className="w-10 h-10 sm:w-12 sm:h-12 object-cover rounded-full mx-4  shadow" src="https://images.unsplash.com/photo-1607789382281-1152591ec0da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80" alt="avatar"></img>
+                      </div>
+                          <a href="https://twitter.com/GressierCosme1" target="_blank" className="text-gray-500">@GressierCosme1</a>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+      </main>
 
       <footer className={styles.footer}>
         <b>© 2021 Copyright Taito All Rights Reserved.</b>
