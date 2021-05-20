a12d73c1557973e376c77879e5da308bcf5ff38a
Taito <taito.tanaka1211@gmail.com>
2021-05-20 23:46:53 +0900

    modify card design

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -27,25 +27,55 @@ export default function Home() {
           </div>
         </div>
 
+{/* card---------------------------------------- */}
+        <div className="mt-10">
         <div className="transition duration-500 ease-in-out bg-blue-600 hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 ...">
         <div className="m-auto px-4 py-8 max-w-xl">
           <div className="bg-white shadow-2xl" >
               <div>
-                  <img src="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"></img>
+                  <Image src="/img/tabi.jpg"
+                  alt="tabi_img"
+                  layout="responsive"
+                  width={600}
+                  height={400}>
+                  </Image>
               </div>
               <div className="px-4 py-2 mt-2 bg-white">
-                  <h2 className="font-bold text-2xl text-gray-800">Who uses Tailwind?</h2>
-                      <p className="sm:text-sm text-xs text-gray-700 px-2 mr-1 my-3">
-                          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora reiciendis ad architecto at aut placeat quia, minus dolor praesentium officia maxime deserunt porro amet ab debitis deleniti modi soluta similique...
+                  <h2 className="font-bold text-4xl text-gray-800">What is Tabi?</h2>
+                      <p className="sm:text-lg text-xs text-gray-700 px-2 mr-1 my-3">
+                      Tabi Shoes are one of the traditional Japanese garments that are worn directly on the feet when wearing kimono.
+                      Tabi Shoes are made up of an instep and a sole. The instep is divided into an inner instep for 
+                      the thumb and an outer instep for the other four toes, and is wrapped around the ankle.
                       </p>
-                  <div className="user flex items-center -ml-3 mt-8 mb-4">
-                      <div className="user-logo">
-                          <img className="w-10 h-10 sm:w-12 sm:h-12 object-cover rounded-full mx-4  shadow" src="https://images.unsplash.com/photo-1607789382281-1152591ec0da?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80" alt="avatar"></img>
-                      </div>
-                          <a href="https://twitter.com/GressierCosme1" target="_blank" className="text-gray-500">@GressierCosme1</a>
-                  </div>
               </div>
           </div>
+      </div>
+      </div>
+      </div>
+
+{/* card-------------------------------------------------------------------------------------------- */}
+      <div className="mt-10">
+      <div className="transition duration-500 ease-in-out bg-blue-600 hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 ...">
+        <div className="m-auto px-4 py-8 max-w-xl">
+          <div className="bg-white shadow-2xl" >
+              <div>
+                  <Image src="/img/tabi.jpg"
+                  alt="tabi_img"
+                  layout="responsive"
+                  width={600}
+                  height={400}>
+                  </Image>
+              </div>
+              <div className="px-4 py-2 mt-2 bg-white">
+                  <h2 className="font-bold text-4xl text-gray-800">History of Tabi</h2>
+                      <p className="sm:text-lg text-xs text-gray-700 px-2 mr-1 my-3">
+                      Tabi Shoes are one of the traditional Japanese garments that are worn directly on the feet when wearing kimono.
+                      Tabi Shoes are made up of an instep and a sole. The instep is divided into an inner instep for 
+                      the thumb and an outer instep for the other four toes, and is wrapped around the ankle.
+                      </p>
+              </div>
+          </div>
+      </div>
       </div>
       </div>
 
