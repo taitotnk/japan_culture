816e52ba31465ee560d9c36a84546f72702e0858
Taito <taito.tanaka1211@gmail.com>
2021-05-22 14:40:03 +0900

    ローカルデータをmapで表示

---
+++ src/data/data.js
@@ -0,0 +1,14 @@
+export const card = [
+    {
+        img: "/img/tabi.jpg",
+        sub_title: "What is Tabi?",
+        description: "Tabi Shoes are one of the traditional Japanese garments that are worn directly on the feet when wearing kimono. Tabi Shoes are made up of an instep and a sole. The instep is divided into an inner instep for the thumb and an outer instep for the other four toes, and is wrapped around the ankle."
+    },
+
+    {
+        img: "/img/tabi.jpg",
+        sub_title: "History of Tabi",
+        description: ""
+    },
+
+]
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
-
+import { card } from '../data/data'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ cardContents }) {
   return (
     <div className={styles.container}>
       <Head>
@@ -28,56 +28,33 @@ export default function Home() {
         </div>
 
 {/* card---------------------------------------- */}
-        <div className="mt-10">
-        <div className="transition duration-500 ease-in-out bg-blue-600 hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 ...">
-        <div className="m-auto px-4 py-8 max-w-xl">
-          <div className="bg-white shadow-2xl" >
-              <div>
-                  <Image src="/img/tabi.jpg"
-                  alt="tabi_img"
-                  layout="responsive"
-                  width={600}
-                  height={400}>
-                  </Image>
-              </div>
-              <div className="px-4 py-2 mt-2 bg-white">
-                  <h2 className="font-bold text-4xl text-gray-800">What is Tabi?</h2>
-                      <p className="sm:text-lg text-xs text-gray-700 px-2 mr-1 my-3">
-                      Tabi Shoes are one of the traditional Japanese garments that are worn directly on the feet when wearing kimono.
-                      Tabi Shoes are made up of an instep and a sole. The instep is divided into an inner instep for 
-                      the thumb and an outer instep for the other four toes, and is wrapped around the ankle.
-                      </p>
-              </div>
-          </div>
-      </div>
-      </div>
-      </div>
+        {cardContents.map( cardContent => (
+          <div className="mt-10">
+          <div className="transition duration-500 ease-in-out bg-blue-600 hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 ...">
+          <div className="m-auto px-4 py-8 max-w-xl">
+            <div className="bg-white shadow-2xl" >
+                <div>
+                    <Image src={cardContent.img}
+                    alt="tabi_img"
+                    layout="responsive"
+                    width={600}
+                    height={400}>
+                    </Image>
+                </div>
+                <div className="px-4 py-2 mt-2 bg-white">
+                    <h2 className="font-bold text-4xl text-gray-800">{cardContent.sub_title}</h2>
+                        <p className="sm:text-lg text-xs text-gray-700 px-2 mr-1 my-3">
+                          {cardContent.description}
+                        </p>
+                </div>
+            </div>
+        </div>
+        </div>
+        </div>
+        ))}
 
-{/* card-------------------------------------------------------------------------------------------- */}
-      <div className="mt-10">
-      <div className="transition duration-500 ease-in-out bg-blue-600 hover:bg-red-600 transform hover:-translate-y-1 hover:scale-110 ...">
-        <div className="m-auto px-4 py-8 max-w-xl">
-          <div className="bg-white shadow-2xl" >
-              <div>
-                  <Image src="/img/tabi.jpg"
-                  alt="tabi_img"
-                  layout="responsive"
-                  width={600}
-                  height={400}>
-                  </Image>
-              </div>
-              <div className="px-4 py-2 mt-2 bg-white">
-                  <h2 className="font-bold text-4xl text-gray-800">History of Tabi</h2>
-                      <p className="sm:text-lg text-xs text-gray-700 px-2 mr-1 my-3">
-                      Tabi Shoes are one of the traditional Japanese garments that are worn directly on the feet when wearing kimono.
-                      Tabi Shoes are made up of an instep and a sole. The instep is divided into an inner instep for 
-                      the thumb and an outer instep for the other four toes, and is wrapped around the ankle.
-                      </p>
-              </div>
-          </div>
-      </div>
-      </div>
-      </div>
+{/* ------------------------------------------------------ */}
+
 
       </main>
 
@@ -86,4 +63,12 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      cardContents: card,
+    },
+  }
 }
